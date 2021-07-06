30e03f4ce1c621c8e4ea00506b155c9e935402db
ashleyjhkoo <ashleyjhkoo@gmail.com>
2021-07-06 03:06:32 -0700

    modify the script.js

--- Module03_Advanced/js/script.js
+++ Module03_Advanced/js/script.js
@@ -7,44 +7,19 @@
 //    });
 // });
 
-// $(document).ready(function () {
-//     $(document).click(function (event) {
-//         var clickover = $(event.target);
-//         var _opened = $(".navbar-collapse").hasClass("navbar-collapse in");
-//         if (_opened === true && !clickover.hasClass("navbar-toggle")) {
-//             $("button.navbar-toggle").click();
-//         }
-//     });
+
+// jQuery(document.body).on('click', function(ev){
+//     if(jQuery(ev.target).closest('.navbar-collapse').length) return; // Not return false
+
+//     jQuery('.navbar-collapse').collapse('hide');
 // });
 
+$(document.body).on('click', function(ev){
+    if($(ev.target).closest('.navbar-collapse').length) {
+       // alert($(ev.target).closest('.navbar-collapse').length);	
+       return; // Not return false
+    }
 
-$(document).ready(function(){
-	$('body').click(function(e){
-		var clicked_class=e.target.getAttribute("class");
-		if(clicked_class==="navbar-toggler"||clicked_class==="navbar-toggler-icon"){
-            //$(".navbar-collapse").toggle();
-        }
-        else if(clicked_class==="nav-link"){
-        	$(".navbar-toggler").click();
-        }
-        else{
-        	if($(".navbar-collapse").hasClass("show")==true){
-        		$(".navbar-toggler").click();
-        	}
-        }
-        /*
-        if(clicked_class==="navbar-collapse"||$(e.target).parents(".navbar-collapse").length>0)
-        {
-            $(".navbar-collapse").toggle();
-        }
-        else
-        {
-            if($(".navbar-collapse").hasClass("show")==true)
-            {
-                alert("click");
-                $(".navbar-collapse").toggle();
-            }
-        }
-        */
-    });
+    $('.navbar-collapse').collapse('hide');
+    // alert($(ev.target).closest('.navbar-collapse').length);	
 });
